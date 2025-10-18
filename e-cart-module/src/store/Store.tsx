9a4e94a6d45a3f1fbd3e3bd93bc06b7a5a56035c
Ashwanth119 <ashwanthth132@gmail.com>
2025-10-18 08:26:40 +0530

    emptied the branch

--- e-cart-module/src/store/Store.tsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import Reducer from "../reducers/Reducer";
-
-const Store = configureStore({
-  reducer: Reducer,
-});
-
-export type appState = ReturnType<typeof Store.getState>
-export default Store;
